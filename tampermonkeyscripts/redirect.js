30536b7957bb2b77c4c1e32e63cc7eeeff732ea5
Shane White <shane.p.white@gmail.com>
2018-12-03 11:41:49 -0600

    Added Tamper Monkey Scripts

---
+++ tampermonkeyscripts/redirect.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Redirect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://connect.thesixfigurementors.com/sp/mlm-alternative/sales-pages/?id=9d6b1d694e2a436648e8464cf6f2f495d19afa80
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+window.location.href = 'http://thesfm.com/timlowjk/?pg=mlm-alternative&ar=awlist5189954&t=shane-test';
+    // Your code here...
+})();
